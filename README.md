9cd9d876ee380b6c7b6d919abf82928cd4338f8a
Kaleb Olson <kaleb.olson@gmail.com>
2016-06-17 17:18:12 -0600

    Intial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Qwerty Muscle Memory Machine
+
+The aim of this simple project is to help me become fluent in qwerty so I can switch back and forth between it and dvorak.
+
+
---
+++ qwertymmm.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import sys
+from pyfiglet import Figlet
+import curses
+import random
+
+FONT = Figlet(font="big")
+KEYBOARD = r"""',.pyfgcrl/=aoeuidhtns-;qjkxbmwvz"""
+
+def main(stdscr):
+    try:
+        my, mx = stdscr.getmaxyx()
+
+        curses.curs_set(0)
+
+        curr  = random.choice(KEYBOARD)
+        while True:
+            stdscr.clear()
+            stdscr.box()
+            fig = FONT.renderText(curr.upper()).splitlines()
+            for y, line in enumerate(fig):
+                stdscr.addstr(my/2 - len(fig)/2 + y, mx/2 - len(line)/2, line)
+            if chr(stdscr.getch()) == curr:
+                curr  = random.choice(KEYBOARD)
+            stdscr.refresh()
+    except KeyboardInterrupt:
+        sys.exit()
+
+
+if __name__ == "__main__":
+    curses.wrapper(main)
+
